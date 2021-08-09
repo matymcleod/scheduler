bb7fee3af115de837d8b415a3064bb8974135a9a
Mathew <matymcleod@gmail.com>
2021-08-08 18:07:00 -0700

    adding promises and refactoring Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,53 +3,9 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointments";
 import axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Oliver McLeod",
-      interviewer: {
-        id: 1,
-        name: "Bob Ross",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Liz Krieg",
-      interviewer: {
-        id: 1,
-        name: "Noam Chomski",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm"
-  }
-];
+
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -70,13 +26,14 @@ export default function Application(props) {
       axios.get('/api/interviewers')
     ]).then((all) => {
       
-      // const [days, apppointments, interviewers] = all;
       const days = all[0].data;
       const appointments = all[1].data;
       const interviewers = all[2].data;
       setState(prev => ({...prev, days, appointments, interviewers }));
     });
     }, [])
+
+    const appointments = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -89,10 +46,10 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
         <DayList
-    days={state.days}
-    day={state.day}
-    setDay={setDay}
-    />
+          days={state.days}
+          day={state.day}
+          setDay={setDay}
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
