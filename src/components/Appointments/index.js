cbb796a00957b3a341ec71aab4b921350caafaff
Mathew <matymcleod@gmail.com>
2021-08-09 15:24:53 -0700

    modifying Appointment component to add EMPTY and SHOW states

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,14 +1,39 @@
 import React from "react";
 import "components/Appointments/styles.scss";
-import Header from "./Header"
-import Show from "./Show"
-import Empty from "./Empty"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
-  
-  return <article className="appointment">
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
-    <Header time={props.time} />
-    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+  return (
+  <article className="appointment">
+  
+    <Header 
+    time={props.time} />
+
+    {mode === EMPTY && 
+    <Empty
+      onAdd={() =>
+        transition(CREATE)
+      }
+    />
+    }
+
+    {mode === SHOW && (
+      <SHOW
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+      />
+    )}
+
+
   </article>
-}
+};
