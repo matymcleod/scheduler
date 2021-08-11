84f23892b5dee6653fed646185a7af948318f3d9
Mathew <matymcleod@gmail.com>
2021-08-10 17:09:55 -0700

    implementing error handeling

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -5,7 +5,8 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Confirm from "./Confirm";
-import Status from "./Status"
+import Status from "./Status";
+import Error from "./Error";
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -15,12 +16,13 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const CONFIRM = "CONFIRM";
 const DELETING = "DELETING";
+const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 
 export default function Appointment(props) {
 
-
-  console.log('this is index ----------:', props);
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -36,15 +38,19 @@ export default function Appointment(props) {
     props
     .bookInterview(props.id, interview)
     .then(() => transition(SHOW))
+    .catch(error => transition(ERROR_SAVE, true));
   }
 
-  function remove(event) {
+  function destroy(event) {
     transition(DELETING, true);
     props
       .cancelInterview(props.id)
       .then(() => {
-        transition(SHOW);
+        transition(EMPTY);
       })
+      .catch(error => {
+        transition(ERROR_DELETE, true)
+      });
   }
 
 
@@ -68,6 +74,7 @@ export default function Appointment(props) {
       student={props.interview.student}
       interviewer={props.interview.interviewer}
       onDelete={() => transition(CONFIRM)}
+      onEdit={() => transition(EDIT)}
       />
     )}
 
@@ -89,8 +96,32 @@ export default function Appointment(props) {
       <Confirm
         message={"Confirm..."}
         onCancel={back}
-        onConfirm={remove}
+        onConfirm={destroy}
       />
+    )}
+
+    {mode === EDIT && (
+      <Form
+      name={props.interview.student}
+      interviewers={props.interviewers}
+      interviewer={props.interview.interviewer.id}
+      onCancel={back}
+      onSave={save}
+      />
+    )}
+
+    {mode === ERROR_SAVE && (
+      <Error
+        message={"Saving error"}
+        onClose={back}
+        />
+    )}
+
+    {mode === ERROR_DELETE && (
+      <Error
+        message={"Deleting error"}
+        onClose={back}
+        />
     )}
 
 
